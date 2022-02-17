a05b05aacb8015e03c3d18c2132f0b2b77ced871
jason.yang <jason.yang@bybit.com>
2022-02-17 21:39:17 +0800

    add data base

---
+++ database/amm_profit_loss.py
@@ -0,0 +1,13 @@
+from helper.db_session import execute
+
+
+# 查询用户钱包记录  type 18 为返还手续费记录
+def insert_records(beta, debt_ratio, liquidity_ratio, base_reserves_begin, market_price_begin, user_a_position,
+                   liquidate_price, user_b_position, market_price_after_b, profit_loss):
+    sql = 'INSERT into amm_profit_loss (beta,debt_ratio,liquidity_ratio,base_reserves_begin,market_price_begin,user_a_position,liquidate_price,user_b_position,market_price_after_b,profit_loss) VALUES(:beta,:debt_ratio,:liquidity_ratio,:base_reserves_begin,:market_price_begin,:user_a_position,:liquidate_price,:user_b_position,:market_price_after_b,:profit_loss)'
+    results = execute(sql=sql, params={'beta': beta, 'debt_ratio': debt_ratio, 'liquidity_ratio': liquidity_ratio,
+                                       'base_reserves_begin': base_reserves_begin,
+                                       'market_price_begin': market_price_begin, 'user_a_position': user_a_position,
+                                       'liquidate_price': liquidate_price, 'user_b_position': user_b_position,
+                                       'market_price_after_b': market_price_after_b, 'profit_loss': profit_loss})
+    return results
--- scripts/config_contract.py
+++ scripts/config_contract.py
@@ -19,10 +19,14 @@ def getBeta():
     beta = Iconfig.beta()
     return beta/100
 
+def setBeta(beta):
+    hash_tx = Iconfig.setBeta(beta, {'from': userA})
+    return hash_tx
+
 def main():
     # t = chain.get_transaction('0xc8d5fee409163ea4cac15cff17a629576f87b10adb40e0c2ae70ef8504fe47a7')
     # print(t.events)
     print(getBeta())
     print(Iconfig.tradingSlippage())
-    Iconfig.setBeta(100, {'from': userA});
+
 
