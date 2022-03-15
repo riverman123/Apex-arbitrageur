320a5c4d5f303b74af55dbeac45aa223cb3ed218
longdacao <longdacao@163.com>
2022-03-15 17:29:32 +0800

    fix code

--- scripts/aave.py
+++ scripts/aave.py
@@ -23,6 +23,9 @@ querySql = """query($id: String!) {
                             id
                         }
                     }
+                    userReserve{
+                        currentTotalDebt
+                    }
                     
                 }
     
@@ -55,21 +58,46 @@ def getInterest(userAddress,URL=url):
     df_borrow_data = json_data['data']['users'][0]['borrowHistory']
     df_repay_data = json_data['data']['users'][0]['repayHistory']
 
+    total_borrow = {}
     # amount need to be summed by loop in case of overflow : https://github.com/pandas-dev/pandas/issues/18842
-    total_borrow = 0
     for borrowRecord in df_borrow_data:
-        total_borrow += int(borrowRecord['amount'])
+        symbol = borrowRecord["reserve"]["symbol"]
+        decimals = int(borrowRecord["reserve"]["decimals"])
+        vToken = borrowRecord["reserve"]["vToken"]["id"]
+        currentTotalDebt = borrowRecord["userReserve"]["currentTotalDebt"]
 
-    total_repay = 0
+        if symbol not in total_borrow:
+            total_borrow[symbol] = {"amount": 0}
+
+        if "decimals" not in total_borrow[symbol]:
+            total_borrow[symbol]["decimals"] = decimals
+
+        if "vToken" not in total_borrow[symbol]:
+            total_borrow[symbol]["vToken"] = vToken
+        
+        if "currentTotalDebt" not in total_borrow[symbol]:
+            total_borrow[symbol]["currentTotalDebt"] = int(currentTotalDebt)
+
+        total_borrow[symbol]["amount"] += int(borrowRecord['amount'])
+
+    total_repay = {}
     for repayRecord in df_repay_data:
-        total_repay += int(repayRecord['amount'])
+        symbol = repayRecord["reserve"]["symbol"]
+        
+        if symbol not in total_repay:
+            total_repay[symbol] = {"amount": 0}
 
-    vTokenAddress = df_borrow_data[0]['reserve']['vToken']['id']
-    currentDebt = getCurrentDebt(vTokenAddress,userAddress)
-    totalInterest = (total_repay + currentDebt - total_borrow) / 10 ** int(df_borrow_data[0]['reserve']['decimals'])
-    interestList = {"symbol": df_borrow_data[0]['reserve']['symbol'], "Total Interest": totalInterest }
+        total_repay[symbol]["amount"] += int(repayRecord['amount'])
 
-    return interestList
+    tota_interest = {}
+    for symbol in total_repay.keys():
+        currentDebt = 0
+        if total_borrow[symbol]["currentTotalDebt"] != 0:
+            currentDebt = getCurrentDebt(total_borrow[symbol]["vToken"],userAddress)
+
+        tota_interest[symbol] = (total_repay[symbol]["amount"] + currentDebt - total_borrow[symbol]["amount"]) / 10**total_borrow[symbol]["decimals"]
+
+    return tota_interest
 
 def getCurrentDebt(debtTokenAddress,userAddress):
     variableDebtToken = interface.IVariableDebtToken(debtTokenAddress)
@@ -81,3 +109,4 @@ def main():
     matic_url = "https://api.thegraph.com/subgraphs/name/aave/aave-v2-matic"
     interest = getInterest(userAddress,URL=matic_url)
     print(interest)
+    
